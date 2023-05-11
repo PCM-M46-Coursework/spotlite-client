9dbc36ca5a4878f3d5dafc746e2d4a7a0f8c1ddc
ApacheTech Solutions <p_matthews@live.co.uk>
2023-05-11 01:22:19 +0100

    feat: add spotlite layout component

--- src/layout/spotLite/SpotLite.js
+++ src/layout/spotLite/SpotLite.js
@@ -2,11 +2,11 @@ import "./SpotLite.css";
 
 export default function SpotLite({ user, setUser }) {
 	return (
-		<div>
-			{/* Header Component */}
-			{/* Sidebar Component */}
-			{/* ContentPanel Component */}
-			{/* Footer Component */}
-		</div>
+		<section className="layout">
+			<div className="header">Header</div>
+			<div className="leftSide">Sidebar</div>
+			<div className="body">Content Panel</div>
+			<div className="footer">Footer (Will contain media player)</div>
+		</section>
 	);
 }
