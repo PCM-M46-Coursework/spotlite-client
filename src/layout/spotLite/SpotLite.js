6f9ab7ccf15f7e4c91408ef31d2c4603ad45b8f2
ApacheTech Solutions <p_matthews@live.co.uk>
2023-05-11 01:25:49 +0100

    feat: add header component

--- src/layout/spotLite/SpotLite.js
+++ src/layout/spotLite/SpotLite.js
@@ -1,9 +1,12 @@
+import Header from "../header/Header";
 import "./SpotLite.css";
 
 export default function SpotLite({ user, setUser }) {
 	return (
 		<section className="layout">
-			<div className="header">Header</div>
+			<div className="header">
+				<Header user={user} setUser={setUser} />
+			</div>
 			<div className="leftSide">Sidebar</div>
 			<div className="body">Content Panel</div>
 			<div className="footer">Footer (Will contain media player)</div>
