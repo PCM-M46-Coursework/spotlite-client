482b4ce58b242ce88117bb58c3dccfc4f0d38f2b
ApacheTech Solutions <p_matthews@live.co.uk>
2023-05-13 16:30:55 +0100

    fix: allow for multiple cookies on site

--- src/utils/writeCookie.js
+++ src/utils/writeCookie.js
@@ -1,9 +1,16 @@
-export default function writeCookie(key, value, days) {
+export default function writeCookie(cookieName, value, days) {
 	let date = new Date();
 	date.setDate(
 		date.getDate() + (days || parseInt(process.env.REACT_APP_COOKIE_TTL)),
 	);
-	let cookie =
-		(document.cookie = `${key}=${value};expires=${date.toGMTString()};path=/`);
-	return cookie;
+
+	const existingCookies = document.cookie;
+	const newCookie = `${cookieName}=${value};expires=${date.toGMTString()};path=/`;
+
+	// Append the new cookie to the existing cookies
+	document.cookie = existingCookies
+		? `${existingCookies}; ${newCookie}`
+		: newCookie;
+
+	return newCookie;
 }
