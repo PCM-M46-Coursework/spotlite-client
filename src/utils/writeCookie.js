41dff9614e765f2882f629e941cf9a8c67e6f355
ApacheTech Solutions <p_matthews@live.co.uk>
2023-05-14 00:43:18 +0100

    revert: undo breaking change to cookies

--- src/utils/writeCookie.js
+++ src/utils/writeCookie.js
@@ -1,16 +1,9 @@
-export default function writeCookie(cookieName, value, days) {
+export default function writeCookie(key, value, days) {
 	let date = new Date();
 	date.setDate(
 		date.getDate() + (days || parseInt(process.env.REACT_APP_COOKIE_TTL)),
 	);
-
-	const existingCookies = document.cookie;
-	const newCookie = `${cookieName}=${value};expires=${date.toGMTString()};path=/`;
-
-	// Append the new cookie to the existing cookies
-	document.cookie = existingCookies
-		? `${existingCookies}; ${newCookie}`
-		: newCookie;
-
-	return newCookie;
+	let cookie =
+		(document.cookie = `${key}=${value};expires=${date.toGMTString()};path=/`);
+	return cookie;
 }
