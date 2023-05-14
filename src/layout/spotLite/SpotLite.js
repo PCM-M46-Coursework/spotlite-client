c2dc42490895f532b9f7b3ca331a175406f53669
ApacheTech Solutions <p_matthews@live.co.uk>
2023-05-14 02:28:41 +0100

    feat: add sidebar component

--- src/layout/spotLite/SpotLite.js
+++ src/layout/spotLite/SpotLite.js
@@ -2,20 +2,25 @@ import { TrackSearchProvider } from "../../context/trackSearchContext/TrackSearc
 import ContentPanel from "../contentPanel/ContentPanel";
 import Footer from "../footer/Footer";
 import Header from "../header/Header";
+import SideBar from "../sideBar/SideBar";
 import "./SpotLite.css";
 
 export default function SpotLite({ user, setUser }) {
 	return (
 		<TrackSearchProvider>
-			<section className="layout">
-				<div className="header">
+			<section className="page-wrapper">
+				<div className="header-wrapper">
 					<Header user={user} setUser={setUser} />
 				</div>
-				<div className="leftSide">Sidebar</div>
-				<div className="body">
-					<ContentPanel />
+				<div className="main-wrapper">
+					<div className="sidebar-wrapper">
+						<SideBar />
+					</div>
+					<div className="content-panel-wrapper">
+						<ContentPanel />
+					</div>
 				</div>
-				<div className="footer">
+				<div className="footer-wrapper">
 					<Footer />
 				</div>
 			</section>
